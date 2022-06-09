08ca2938db07b94a6233e833be64688b7e6168d9
Philip Michael Raab <peep@inane.co.za>
2022-06-09 16:19:57 +0200

    new exception for configuration errors

---
+++ src/Exception/ConfigurationException.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Inane: Stdlib
+ *
+ * Inane Standard Library
+ *
+ * PHP version 8.1
+ *
+ * @author Philip Michael Raab<peep@inane.co.za>
+ * @package Inane\Stdlib
+ *
+ * @license UNLICENSE
+ * @license https://github.com/inanepain/stdlib/raw/develop/UNLICENSE UNLICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Inane\Stdlib\Exception;
+
+use Inane\Stdlib\Exception\Exception as InaneException;
+
+/**
+ * ConfigurationException
+ *
+ * @package Inane\Stdlib
+ *
+ * @version 0.1.0
+ */
+class ConfigurationException extends InaneException {
+}
