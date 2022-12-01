80c05f89e60054a0e21b1a54c4f3e006059e9fa8
Philip Michael Raab <peep@inane.co.za>
2022-12-01 13:28:50 +0200

    - `Str::pad` Pad to a certain length with character - ArrayKit more tested functions - ArrayObject::randomItem

--- src/ArrayObject.php
+++ src/ArrayObject.php
@@ -25,7 +25,12 @@ namespace Inane\Stdlib;
 use ArrayIterator;
 use ArrayObject as SystemArrayObject;
 
+use function count;
+use function current;
 use function is_array;
+use function key;
+use function next;
+use function random_int;
 
 /**
  * ArrayObject
@@ -48,9 +53,9 @@ class ArrayObject extends SystemArrayObject {
 
         foreach ($array as $key => $value) if (is_array($value)) $array[$key] = new static($value);
         parent::__construct(
-                $array,
-                static::ARRAY_AS_PROPS,
-                ArrayIterator::class
+            $array,
+            static::ARRAY_AS_PROPS,
+            ArrayIterator::class
         );
     }
 
@@ -106,5 +111,19 @@ class ArrayObject extends SystemArrayObject {
         if (is_array($value)) $value = new static($value);
 
         parent::offsetSet($key, $value);
+    }
+
+    /**
+     * Random Item
+     *
+     * @return array
+     */
+    public function randomItem(): array {
+        $data = $this->getArrayCopy();
+        $r = random_int(0, count($data) - 1);
+        for ($i = 0; $i < $r; $i++)
+            next($data);
+
+        return [key($data) => current($data)];
     }
 }
