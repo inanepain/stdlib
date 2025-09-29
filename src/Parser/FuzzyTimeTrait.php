2a5226b27bbe2afcfc78bd42c310c8aba0833797
Philip Michael Raab <philip@cathedral.co.za>
2025-09-29 19:59:08 +0200

    Fuzzy Time
    - Fuzzy Time trait added

---
+++ src/Parser/FuzzyTimeTrait.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * Inane: Stdlib
+ *
+ * Common classes, tools and utilities used throughout the inanepain libraries.
+ *
+ * $Id$
+ * $Date$
+ *
+ * PHP version 8.4
+ *
+ * @author Philip Michael Raab<philip@cathedral.co.za>
+ * @package inanepain\stdlib
+ * @category stdlib
+ *
+ * @license UNLICENSE
+ * @license https://unlicense.org/UNLICENSE UNLICENSE
+ *
+ * _version_ $version
+ */
+
+declare(strict_types=1);
+
+namespace Inane\Stdlib\Parser;
+
+/**
+ * Trait FuzzyTime
+ * 
+ * // todo: version bump
+ */
+trait FuzzyTimeTrait {
+    /**
+     * @var array minutes to words mappings.
+     */
+    private const array words = [
+        0  => "o'clock",
+        5  => 'five past',
+        10 => 'ten past',
+        15 => 'quarter past',
+        20 => 'twenty past',
+        25 => 'twenty-five past',
+        30 => 'half past',
+        35 => 'twenty-five to',
+        40 => 'twenty to',
+        45 => 'quarter to',
+        50 => 'ten to',
+        55 => 'five to'
+    ];
+
+    /**
+     * @var array number to words mappings.
+     */
+    private const array map = [
+        1 => 'one',
+        2 => 'two',
+        3 => 'three',
+        4 => 'four',
+        5 => 'five',
+        6 => 'six',
+        7 => 'seven',
+        8 => 'eight',
+        9 => 'nine',
+        10 => 'ten',
+        11 => 'eleven',
+        12 => 'twelve'
+    ];
+
+    /**
+     * Converts a DateTime into words.
+     * 
+     * @param null|\DateTime|\Inane\Datetime\Timestamp $time time to convert.
+     * 
+     * @return string time words.
+     */
+    public static function fuzzyClock(null|\DateTime|\Inane\Datetime\Timestamp $time = null): string {
+        $time = $time ?? new Timestamp()->getDateTime();
+        if ($time instanceof Timestamp) $time = $time->getDateTime();
+        $hour = (int) $time->format('G');
+        $minute = (int) $time->format('i');
+
+        // Round minutes to nearest 5
+        $minute = (int)(5 * round($minute / 5));
+
+        if ($minute >= 60) {
+            $minute = 0;
+            $hour++;
+        }
+
+        return match (true) {
+            $minute === 0 => self::numToWords($hour % 12 ?: 12) . " o'clock",
+            $minute <= 30 => self::words[$minute] . ' ' . self::numToWords($hour % 12 ?: 12),
+            default => self::words[$minute] . ' ' . self::numToWords(($hour + 1) % 12 ?: 12),
+        };
+    }
+
+    /**
+     * converts an integer into words.
+     * 
+     * @param int $num number to convert.
+     * 
+     * @return string words number
+     */
+    private static function numToWords(int $num): string {
+        return self::map[$num];
+    }
+}
