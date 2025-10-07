82567cecb0f3691e568b1d456fc90ab56a9e1e6b
Philip Michael Raab <philip@cathedral.co.za>
2025-10-07 22:30:29 +0200

    json
    - encode easy option escape

--- src/Json.php
+++ src/Json.php
@@ -52,6 +52,7 @@ use const true;
 /**
  * JSON en/decoder
  *
+ * @todo: version bump
  * @version 0.2.0
  */
 class Json {
@@ -105,10 +106,13 @@ class Json {
      * OPTIONS:
      *  - (bool) [pretty=false] format result
      *  - (bool) [numeric=true] check for numeric values
+     *  - (bool) [escape=true] unescape slashes and unicode
      *  - (bool) [hex=false] encode ',<,>,",& are encoded to \u00*
      *  - (int) [flags=0] Bitmask consisting of JSON_FORCE_OBJECT, JSON_HEX_QUOT, JSON_HEX_TAG, JSON_HEX_AMP, JSON_HEX_APOS, JSON_INVALID_UTF8_IGNORE, JSON_INVALID_UTF8_SUBSTITUTE, JSON_NUMERIC_CHECK, JSON_PARTIAL_OUTPUT_ON_ERROR, JSON_PRESERVE_ZERO_FRACTION, JSON_PRETTY_PRINT, JSON_UNESCAPED_LINE_TERMINATORS, JSON_UNESCAPED_SLASHES, JSON_UNESCAPED_UNICODE, JSON_THROW_ON_ERROR. The behaviour of these constants is described on the JSON constants page.
      *
+     * @todo: version bump
 	 * @version 0.1.3 - [options->hex=false]
+	 * @version version bump - [options->escape=true]
 	 *
      * @param mixed $data The value being encoded. Can be any type except a resource.
      * @param array $options encoding options
@@ -119,13 +123,15 @@ class Json {
         $options += [
             'pretty' => false,
             'numeric' => true,
+            'escape' => true,
             'hex' => false,
             'flags' => 0,
         ];
 
-        ['pretty' => $pretty, 'numeric' => $numeric, 'hex' => $hex, 'flags' => $flags] = $options;
+        ['pretty' => $pretty, 'numeric' => $numeric, 'escape' => $escape, 'hex' => $hex, 'flags' => $flags] = $options;
 
         $flags |= $hex ? JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP : 0;
+        $flags |= $escape ? JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES : 0;
         $flags |= $numeric ? JSON_NUMERIC_CHECK : 0;
         $flags |= $pretty ? JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES : 0;
 
