ca51c66a6871c73a1d1db614c453a3e0a8c346e6
Philip Michael Raab <philip@cathedral.co.za>
2026-02-23 20:05:38 +0200

    Exception * added `NotFoundException` for missing required items.

---
+++ src/Exception/NotFoundException.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Inane: Stdlib
+ *
+ * Common classes that cover a wide range of cases that are used throughout the inanepain libraries.
+ *
+ * $Id$
+ * $Date$
+ *
+ * PHP version 8.4
+ *
+ * @author Philip Michael Raab<philip@cathedral.co.za>
+ * @package inanepain\stdlib
+ * @category stdlib
+ *
+ * @license UNLICENSE
+ * @license https://unlicense.org/UNLICENSE UNLICENSE
+ *
+ * _version_ $version
+ */
+
+declare(strict_types=1);
+
+namespace Inane\Stdlib\Exception;
+
+/**
+ * NotFoundException
+ *
+ * Required item not found.
+ *
+ * @version 0.1.0
+ */
+class NotFoundException extends Exception {
+    protected $code = 760;
+}
